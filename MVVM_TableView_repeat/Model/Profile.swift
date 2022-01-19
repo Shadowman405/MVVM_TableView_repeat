90ca08adaf0dc5f219426b1ee8ee8459efcd180e
Maxim Mitin <maximus405@gmail.com>
2022-01-19 23:03:24 +0300

    MVC project created and ready for MVVM refactoring

---
+++ MVVM_TableView_repeat/Model/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM_TableView_repeat
+//
+//  Created by Maxim Mitin on 19.01.22.
+//
+
+import Foundation
+
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM_TableView_repeat/View/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  MVVM_TableView_repeat
+//
+//  Created by Maxim Mitin on 19.01.22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+}
